eb8329e83385f328913cfcf41009a654d3f670fe
switchcopa <100treyholds@gmail.com>
2025-08-26 12:48:06 +0100

    WIP: add config and game.py for Game class

---
+++ src/core/game.py
@@ -0,0 +1,22 @@
+import pygame 
+from config import WINDOW_WIDTH, WINDOW_HEIGHT
+
+class Game: 
+    def __init__(self, background_color, caption, window_width, window_height): 
+        self.background_color = background_color 
+        self.caption = caption
+        self.screen_width = window_width
+        self.screen_height = window_height
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+
+    def run(self): 
+        pygame.display.set_caption(self.caption)
+        screen.fill(self.background_color) 
+
+        pygame.display.flip()
+
+        running = True
+        while running: 
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT: 
+                    running = False
